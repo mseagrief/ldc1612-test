ed0a32d259ed682dde6b60afd4e70f1f5bcb14fd
Mark Seagrief <mark@seagrief.co.uk>
2020-12-28 22:15:37 +0000

    Tweak limits

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 LDC1612 sensor;
 const u32 DISTANCE_00=44000000;  //do not detect
 const u32 DISTANCE_00_01=60000000;  // distance:<1mm
-const u32 DISTANCE_01_05=45000000;  // distance:1mm~5mm
+const u32 DISTANCE_01_05=47000000;  // distance:1mm~5mm
+//const u32 DISTANCE_01_05=45000000;  // distance:1mm~5mm
 const u32 DISTANCE_05_10=44250000; // distance:5mm~10mm
 const u32 DISTANCE_10_15=44080000; // distance:10mm~15mm
 const u32 DISTANCE_15_20=44020000; // distance:15mm~20mm
@@ -22,11 +23,23 @@ void setup() {
         Serial.println("can't detect sensor!");
         while(1);
     }
+
+    pinMode(LED_BUILTIN, OUTPUT);
+
+    //auto timeConst = 0x0546;
+    auto timeConst = 0x04d6;
+    //auto timeConst = 0xffff;
+
+    auto fref = sensor.fref(CHANNEL_0);
+
+    auto time = (timeConst*16 + 6) / fref;
+    Serial.println("time="+String(time)+"  fref="+String(fref)+"\n");
+
+    sensor.set_conversion_time(CHANNEL_0, timeConst);
 }
 
 void loop() {
-  u32 result_channel1=0;
-  u32 result_channel2=0;
+    u32 result_channel1=0;
  
     /*shows the status of sensor.*/
     //sensor.get_sensor_status();
@@ -39,12 +52,24 @@ void loop() {
     Please suspend the sensor during the test! */
     if(0!=result_channel1)
     {
+        /*Serial.println("raw = "+String(result_channel1));
+
         if(result_channel1<DISTANCE_00)
         {
             Serial.println("There is no coin here!");
-        }
+        }*/
  
-        if(result_channel1>=DISTANCE_00_01)
+        if(result_channel1<DISTANCE_00_01 && result_channel1>=DISTANCE_01_05)
+        //if(result_channel1>=DISTANCE_00_01)
+        {
+            digitalWrite(LED_BUILTIN, HIGH);
+        }
+        else
+        {
+            digitalWrite(LED_BUILTIN, LOW);
+        }
+
+        /*if(result_channel1>=DISTANCE_00_01)
         {
             Serial.println("The distance between the COIN is 0~1mm");
         }
@@ -72,9 +97,9 @@ void loop() {
         if(result_channel1<DISTANCE_15_20&&result_channel1>=DISTANCE_00)
         {
             Serial.println("The distance between the COIN is more than 2mm");
-        }
+        }*/
  
     }
  
-    delay(1000);
+    delay(2);
 }
